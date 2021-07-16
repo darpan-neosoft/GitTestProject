f04452a98faccf3ad6917c52417a7bc977ad79f1
darpan-neosoft <darpan.vasayani@neosoftmail.com>
2021-07-16 18:57:46 +0530

    First Commit

---
+++ GitTestProject/src/com/neosoft/github/GitTest.java
@@ -0,0 +1,14 @@
+package com.neosoft.github;
+
+public class GitTest {
+	public static void main(String args[]) {
+		String code = "SE";
+		if(code=="SE") {
+			System.out.println("Software  Engineer");
+		}
+		else {
+			System.out.println("No Software Engineer");
+		}
+	}
+
+}
